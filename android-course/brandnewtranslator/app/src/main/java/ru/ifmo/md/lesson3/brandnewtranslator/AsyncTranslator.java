96f5b1ad0030bec1b3d8214b256933fe66786897
DimaPhil <dima239philippov@gmail.com>
2014-09-29 13:46:21 +0400

    Translation works now

--- android-course/brandnewtranslator/app/src/main/java/ru/ifmo/md/lesson3/brandnewtranslator/AsyncTranslator.java
+++ android-course/brandnewtranslator/app/src/main/java/ru/ifmo/md/lesson3/brandnewtranslator/AsyncTranslator.java
@@ -3,40 +3,39 @@ package ru.ifmo.md.lesson3.brandnewtranslator;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
 import org.json.JSONObject;
 
-import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
-
-/**
- * Created by vadim on 28/09/14.
- */
 public class AsyncTranslator extends AsyncTask<String, Void, String> {
     protected static final String TAG = "AsyncTranslator";
+    protected static final String MAIN_URL = "https://translate.yandex.net/api/v1.5/tr.json/translate";
+    protected static final String API_KEY = "trnsl.1.1.20140928T165116Z.593c092fb80c5665.489e094f9a1d86b0043990e21a3c5abc1cdce854";
+    protected static final String LANGUAGE = "en-ru";
 
     @Override
     protected String doInBackground(String... strings) {
         Log.d(TAG, "doInBackground() started");
 
         String word = strings[0];
-        String sURL = "http://www.google.ru";
 
         try {
-            URL url = new URL("http://example.com/pages/");
+            URL url = new URL(MAIN_URL + "?key=" + API_KEY + "&text=" + word + "&lang=" + LANGUAGE);
             HttpsURLConnection request = (HttpsURLConnection) url.openConnection();
             request.connect();
 
-            JSONObject obj = new JSONObject(" .... ");
-            String translatedWord = obj.getString("text");
+            BufferedReader streamReader = new BufferedReader(new InputStreamReader((InputStream) request.getContent(), "UTF-8"));
+            StringBuilder responseStrBuilder = new StringBuilder();
+
+            String inputString;
+            while ((inputString = streamReader.readLine()) != null)
+                responseStrBuilder.append(inputString);
+            JSONObject answer = new JSONObject(responseStrBuilder.toString());
+            String translatedWord = answer.get("text").toString();
             return translatedWord;
         } catch (Throwable t) {
-            //throw new RuntimeException();
             return "fail :(";
         }
-        /*JsonParser jp = new JsonParser(); //from gson
-        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //convert the input stream to a json element
-        JsonObject rootobj = root.getAsJsonObject(); //may be an array, may be an object.
-        zipcode=rootobj.get("zipcode").getAsString();//just grab the zipcode*/
     }
 }
