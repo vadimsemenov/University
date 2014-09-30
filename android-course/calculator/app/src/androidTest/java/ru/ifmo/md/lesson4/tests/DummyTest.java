879dd8bfa83ab5ed280ee3db184b8efd722847e1
Sergey Vasilinets <sergeyv@chromium.org>
2014-10-01 01:13:37 +0400

    Add robolectric routine

---
+++ android-course/calculator/app/src/androidTest/java/ru/ifmo/md/lesson4/tests/DummyTest.java
@@ -0,0 +1,23 @@
+package ru.ifmo.md.lesson4.tests;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@Config(emulateSdk = 18)
+@RunWith(RobolectricTestRunner.class)
+public class DummyTest {
+    @Before
+    public void setup() {
+        //do whatever is necessary before every test
+    }
+
+    @Test
+    public void testWhoppingComplex() {
+        Assert.assertTrue(Boolean.TRUE);
+    }
+}
