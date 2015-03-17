5bdfc13382eb18e43b69c8c7f8f4913218967feb
Vadim Semenov <semenov@rain.ifmo.ru>
2015-03-18 02:18:24 +0300

    Function read_until added

--- os-course/lib/helpers.c
+++ os-course/lib/helpers.c
@@ -27,3 +27,23 @@ ssize_t write_(int fd, const void *buf, size_t count) {
   return allready_writed;
 }
 
+ssize_t read_until(int fd, void *buf, size_t count, const char delimiter) {
+  size_t allready_readed = 0;
+  int contains_delimiter = 0;
+  while (allready_readed < count && contains_delimiter == 0) {
+    ssize_t readed = read(fd, buf + allready_readed, count - allready_readed);
+    if (readed == -1) {
+      return readed;
+    }
+    if (readed == 0) {
+      break;
+    }
+    for (size_t i = allready_readed; i < allready_readed + readed; ++i) {
+      if (*((char *) buf + i) == delimiter) {
+        contains_delimiter = 1;
+        break;
+      }
+    }
+  }
+  return allready_readed;
+}
--- os-course/lib/helpers.h
+++ os-course/lib/helpers.h
@@ -6,4 +6,6 @@
 ssize_t read_(int fd, void *buf, size_t count);
 ssize_t write_(int fd, const void *buf, size_t count);
 
+ssize_t read_until(int fd, void *buf, size_t count, const char delimiter);
+
 #endif // OS_HELPERS
