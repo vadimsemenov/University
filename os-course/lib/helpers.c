24d61444e1b57478ab542821f8866daa8ad23c06
Vadim Semenov <semenov@rain.ifmo.ru>
2015-05-29 04:35:29 +0300

    Stupid runpiped added

--- os-course/lib/helpers.c
+++ os-course/lib/helpers.c
@@ -1,5 +1,99 @@
 #include "helpers.h"
 
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+
+/* Task #6 */
+execargs_t *new_execargs(size_t argc) {
+  execargs_t *result = (execargs_t *) malloc(sizeof(execargs_t) + (2 + argc) * sizeof(char *));
+  if (result == NULL) return result;
+  result->argc = argc;
+  result->argv[argc + 1] = NULL;
+  return result;
+}
+
+void set_args(execargs_t *program, char **args) {
+  if (program == NULL) return;
+  memmove(program->argv + sizeof(char *), args, program->argc);
+}
+
+void set_file(execargs_t *program, char *file) {
+  if (program == NULL) return;
+  program->file = file;
+  program->argv[0] = file;
+}
+
+execargs_t *new_bulk_execargs(char *file, size_t argc, char **argv) {
+  execargs_t *result = new_execargs(argc);
+  set_file(result, file);
+  set_args(result, argv);
+  return result;
+}
+
+int exec(execargs_t *args) {
+  return execvp(args->file, args->argv);
+}
+
+int runpiped(execargs_t **programs, size_t n) {
+  int *pipefd = (int *) malloc(2 * n * sizeof(int));
+  pipefd[0] = STDOUT_FILENO;
+  pipefd[2 * n - 1] = STDIN_FILENO;
+  size_t i;
+  for (i = 0; i + 1 < n; ++i) {
+    /* Bind ith end of the pipe with (i+1)th */
+    if (pipe2(pipefd + 1 + 2 * i, O_CLOEXEC) < 0) {
+      /* Fail, close all opened fd */
+      size_t j;
+      for (j = 0; j < 2 * i; ++j) {
+        close(pipefd[j]);
+      }
+      free(pipefd);
+      return -1;
+    }
+  }
+  /* So ith process write end of the pipe is pipefd[2 * (n - 1 - i)]
+     and read end -- pipefd[2 * (n - 1 - i) + 1] */
+  
+  int *childid = (int *) malloc(n * sizeof(int));
+  for (i = 0; i < n; ++i) {
+    childid[i] = fork();
+    if (childid[i] == 0) {
+      if (dup2(pipefd[2 * (n - 1 - i) + 1], STDIN_FILENO) < 0) {
+        _exit(errno);
+      }
+      if (dup2(pipefd[2 * (n - 1 - i)], STDOUT_FILENO) < 0) {
+        _exit(errno);
+      }
+      /* Why we don't need to close all unused fds?.. */
+      return exec(programs[i]);
+    } else if (childid[i] < 0) {
+      size_t j;
+      for (j = 1; j + 1 < 2 * n; ++j) {
+        close(pipefd[j]);
+      }
+      for (j = 0; j < i; ++j) {
+        kill(childid[j], SIGKILL); 
+        waitpid(childid[j], NULL, 0); /* Avoid zombies! Oo */
+      }
+      free(childid);
+      free(pipefd);
+      return -1;
+    }
+  }
+
+  /* Wait for all children termination */
+  wait(NULL);
+
+  free(childid);
+  free(pipefd);
+  return 0;
+}
+
+/* Task #3 */
 int spawn(const char *file, char *const argv[]) {
   pid_t child_id = fork();
   if (child_id == -1) {
@@ -20,6 +114,7 @@ ERROR:
   return -1;
 }
 
+/* Task #1 */
 ssize_t read_(int fd, void *buf, size_t count) {
   size_t allready_readed = 0;
   while (allready_readed < count) {
@@ -47,6 +142,7 @@ ssize_t write_(int fd, const void *buf, size_t count) {
   return allready_writed;
 }
 
+/* Task #2 */
 ssize_t read_until(int fd, void *buf, size_t count, const char delimiter) {
   size_t allready_readed = 0;
   int contains_delimiter = 0;
--- os-course/lib/helpers.h
+++ os-course/lib/helpers.h
@@ -5,11 +5,26 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+/* Task #1 */
 ssize_t read_(int fd, void *buf, size_t count);
 ssize_t write_(int fd, const void *buf, size_t count);
 
+/* Task #2 */
 ssize_t read_until(int fd, void *buf, size_t count, const char delimiter);
 
+/* Task #3 */
 int spawn(const char *file, char *const argv[]);
+
+/* Task #5 */
+struct execargs_t {
+  char *file;
+
+  size_t argc;
+  char **argv; 
+  /** Arguments in fromat: [file, arg0, arg1, ..., argn, NULL] */
+};
+
+int exec(execargs_t *args);
+int runpiped(execargs_t **programs, size_t n);
 
 #endif // OS_HELPERS
