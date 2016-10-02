7101a974d5f5e6a199123f2813bc4fc7f073dc99
Vadim Semenov <vadim.pillarbox@gmail.com>
2016-10-02 19:33:09 +0300

    [fp][hw3] Finish Phantom Coin task

--- functional-programming-course/hw-3/PhantomCoins.hs
+++ functional-programming-course/hw-3/PhantomCoins.hs
@@ -1,6 +1,7 @@
 module PhantomCoins where
 
 newtype Coin color = Coin { getCoin :: Int }
+  deriving Show
 
 instance Monoid (Coin color) where
   mempty                    = Coin 0
@@ -13,3 +14,21 @@ instance Num (Coin color) where
   signum                = Coin . signum . getCoin
   negate                = Coin . negate . getCoin
   fromInteger           = Coin . fromIntegral
+
+cmp :: (Estimated c1, Estimated c2) => Coin c1 -> Coin c2 -> Ordering
+cmp x y = case compare (estimate x) (estimate y) of
+  LT -> LT
+  GT -> GT
+  EQ -> compare (getCoin x) (getCoin y)
+
+class Estimated a where
+  estimate :: Coin a -> Double
+
+data Blue
+data Red
+
+instance Estimated Blue where
+  estimate _ = 1
+
+instance Estimated Red where
+  estimate _ = 2
